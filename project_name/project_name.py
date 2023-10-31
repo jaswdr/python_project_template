a9042bc4b594e47ad59c077518386c181780a5e8
Jonathan Schweder <jonathanschweder@amazon.com>
2023-10-31 11:41:08 +0000

    Initial commit

---
+++ project_name/project_name.py
@@ -0,0 +1,2 @@
+class ProjectName:
+    pass
---
+++ test/test_project_name.py
@@ -0,0 +1,7 @@
+from project_name.project_name import ProjectName
+
+
+def test_can_intantiate():
+    """Test that we can instantiate the class."""
+    project_name = ProjectName()
+    assert project_name is not None
